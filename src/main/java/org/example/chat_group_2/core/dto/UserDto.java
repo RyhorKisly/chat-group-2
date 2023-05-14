5981dbcac26c859e5767d980df252d41d2b54a9b
Grisha <ruhorkisly@gmail.com>
2023-05-14 21:21:53 +0300

    добавил поля в UserDto

--- src/main/java/org/example/chat_group_2/core/dto/UserDto.java
+++ src/main/java/org/example/chat_group_2/core/dto/UserDto.java
@@ -2,5 +2,10 @@ package org.example.chat_group_2.core.dto;
 
 public class UserDto {
 //    hello
-//    учимся мерджить хехе
+    private String login;
+    private String passWord;
+    private String fullName;
+    private String birthDay;
+    private String registrationDay;
+    private String role;
 }
