7b6300b5a06348eefb3f7f48601c7b64fe6b0dcf
Grisha <ruhorkisly@gmail.com>
2023-05-16 22:23:35 +0300

    сервлет Главная, начало

---
+++ src/main/java/org/example/chat_group_2/web/servlets/ui/UiServlet.java
@@ -0,0 +1,19 @@
+package org.example.chat_group_2.web.servlets.ui;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/ui")
+public class UiServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/Ui.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
