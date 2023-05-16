3c4be6cbf5b9eb97628b23d69d807fcab9d507d7
Grisha <ruhorkisly@gmail.com>
2023-05-16 22:24:07 +0300

    добавил конструктор для admin

--- src/main/java/org/example/chat_group_2/dao/memory/UserMemoryDao.java
+++ src/main/java/org/example/chat_group_2/dao/memory/UserMemoryDao.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserMemoryDao implements IUserDao {
     private final Map<String, UserDto> users = new ConcurrentHashMap<>();
     public UserMemoryDao() {
-        users.put("Grisha", new UserDto("Grisha", "Grisha", "Кислый", "Григорий", "Дмитриевич", "13.08.1993г.", "16.05.2023г. 16:44:35", "Admin"));
+        users.put("Grisha", new UserDto("Grisha", "Grisha","Admin"));
 //        users.put("Dima", )
 //        users.put("Vladislav", )
 //        users.put("Zenia", )
