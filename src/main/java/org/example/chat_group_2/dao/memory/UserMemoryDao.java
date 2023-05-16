16ea697056f243df51e87688a60849c743b55368
Grisha <ruhorkisly@gmail.com>
2023-05-16 17:25:08 +0300

    добавил UserDaoFactory и изменил Dao

--- src/main/java/org/example/chat_group_2/dao/memory/UserMemoryDao.java
+++ src/main/java/org/example/chat_group_2/dao/memory/UserMemoryDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class UserDao implements IUserDao {
+public class UserMemoryDao implements IUserDao {
     private final Map<String, UserDto> users = new ConcurrentHashMap<>();
     @Override
     public List<UserDto> get() {
