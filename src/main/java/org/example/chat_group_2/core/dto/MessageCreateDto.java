2db09f5f44c286f58acb6f2c2f290a9a3245341e
Vlad <mitskevichvlm@gmail.com>
2023-05-16 20:02:38 +0300

    feat: add MessageCreateDto

---
+++ src/main/java/org/example/chat_group_2/core/dto/MessageCreateDto.java
@@ -0,0 +1,37 @@
+package org.example.chat_group_2.core.dto;
+
+public class MessageCreateDto {
+    private UserDto from;
+    private UserDto to;
+    private String letter;
+
+    public MessageCreateDto(UserDto from, UserDto to, String letter) {
+        this.from = from;
+        this.to = to;
+        this.letter = letter;
+    }
+
+    public UserDto getFrom() {
+        return from;
+    }
+
+    public void setFrom(UserDto from) {
+        this.from = from;
+    }
+
+    public UserDto getTo() {
+        return to;
+    }
+
+    public void setTo(UserDto to) {
+        this.to = to;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+}
