2bf43311ea022e3bc196ecf2457f5a9f79b14575
Grisha <ruhorkisly@gmail.com>
2023-05-17 23:34:45 +0300

    зменения в jsp

--- src/main/java/org/example/chat_group_2/web/servlets/ui/UiServlet.java
+++ src/main/java/org/example/chat_group_2/web/servlets/ui/UiServlet.java
@@ -8,11 +8,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.example.chat_group_2.core.dto.UserDto;
+import org.example.chat_group_2.service.api.IUserService;
+import org.example.chat_group_2.service.factory.UserServiceFactory;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/ui")
 public class UiServlet extends HttpServlet {
+    private final IUserService userService;
+    public UiServlet() {
+        userService = UserServiceFactory.getInstance();
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
@@ -21,6 +28,8 @@ public class UiServlet extends HttpServlet {
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/Ui.jsp");
             requestDispatcher.forward(req, resp);
         } else {
+            List<UserDto> users = userService.get();
+            req.setAttribute("users", users);
             req.setAttribute("user", dto);
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/UiForUsers.jsp");
             requestDispatcher.forward(req, resp);
@@ -35,6 +44,8 @@ public class UiServlet extends HttpServlet {
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/Ui.jsp");
             requestDispatcher.forward(req, resp);
         } else {
+            List<UserDto> users = userService.get();
+            req.setAttribute("users", users);
             req.setAttribute("user", dto);
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/UiForUsers.jsp");
             requestDispatcher.forward(req, resp);
