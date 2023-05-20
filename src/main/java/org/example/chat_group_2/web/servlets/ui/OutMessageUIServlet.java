c91732a01fb46ba849131d0fdfd938cd1671b798
Vlad <mitskevichvlm@gmail.com>
2023-05-20 18:09:34 +0300

    feat: add OutMessageServlet rename

---
+++ src/main/java/org/example/chat_group_2/web/servlets/ui/OutMessageUIServlet.java
@@ -0,0 +1,18 @@
+package org.example.chat_group_2.web.servlets.ui;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/ui/user/chats/out")
+public class OutMessageUIServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/ui/user/outMessages.jsp").forward(req, resp);
+    }
+}
