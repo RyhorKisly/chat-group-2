d11ed5f1be80fa9915b643e60a7b7b6951986aad
Vlad <mitskevichvlm@gmail.com>
2023-05-20 18:09:21 +0300

    feat: add InMessageServlet rename

--- src/main/java/org/example/chat_group_2/web/servlets/ui/InMessageUIServlet.java
+++ src/main/java/org/example/chat_group_2/web/servlets/ui/InMessageUIServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/ui/user/chats")
-public class InMessageServlet extends HttpServlet {
+public class InMessageUIServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/ui/user/inMessages.jsp").forward(req, resp);
