503a2c7da7b2a65086571a49b9fafe8b1c6bc639
apadi <smolskydima33@gmail.com>
2023-05-21 23:51:32 +0300

    подправил jsp и listener

--- src/main/java/org/example/chat_group_2/web/listeners/ActiveUserListener.java
+++ src/main/java/org/example/chat_group_2/web/listeners/ActiveUserListener.java
@@ -4,19 +4,11 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebListener;
 import jakarta.servlet.http.HttpSessionAttributeListener;
 import jakarta.servlet.http.HttpSessionBindingEvent;
-import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
 
 @WebListener
 public class ActiveUserListener implements HttpSessionListener, HttpSessionAttributeListener {
 
-    @Override
-    public void sessionCreated(HttpSessionEvent e) {
-    }
-
-    @Override
-    public void sessionDestroyed(HttpSessionEvent e) {
-    }
 
     @Override
     public void attributeAdded(HttpSessionBindingEvent event) {
@@ -34,10 +26,6 @@ public class ActiveUserListener implements HttpSessionListener, HttpSessionAttri
             int activeUserCount = (int) servletContext.getAttribute("activeUserCount");
             servletContext.setAttribute("activeUserCount", activeUserCount - 1);
         }
-    }
-
-    @Override
-    public void attributeReplaced(HttpSessionBindingEvent event) {
     }
 }
 
