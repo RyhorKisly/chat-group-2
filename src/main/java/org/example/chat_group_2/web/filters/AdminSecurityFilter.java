63d9a10ecf6ffd640ce2b7dab914f7382fccf0be
Evgeny Sikorski <e.sikorski1@gmail.com>
2023-05-21 09:06:30 +0300

    Добавил фильтры AdminSecurity и UserSecurity

--- src/main/java/org/example/chat_group_2/web/filters/AdminSecurityFilter.java
+++ src/main/java/org/example/chat_group_2/web/filters/AdminSecurityFilter.java
@@ -1,2 +1,34 @@
-package org.example.chat_group_2.web.filters;public class AdminSecurityFilter {
+package org.example.chat_group_2.web.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.example.chat_group_2.core.dto.UserDto;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/ui/admin/*", "/api/admin/*"})
+public class AdminSecurityFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String contextPath = req.getContextPath();
+        HttpSession session = req.getSession();
+        UserDto user = (UserDto) session.getAttribute("user");
+        if((session!=null) && (user !=null)){
+            if (user.getLogin().equals("Admin") && user.getPassword().equals("Admin")){
+                filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+            res.sendRedirect(contextPath + "/");
+            }
+        } else {
+            res.sendRedirect(contextPath + "/signIn");
+        }
+    }
 }
--- src/main/java/org/example/chat_group_2/web/filters/UserSecurityFilter.java
+++ src/main/java/org/example/chat_group_2/web/filters/UserSecurityFilter.java
@@ -1,2 +1,30 @@
-package org.example.chat_group_2.web.filters;public class UserSecurityFilter {
+package org.example.chat_group_2.web.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/ui/user/*", "/api/message"})
+public class UserSecurityFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String contextPath = req.getContextPath();
+        HttpSession session = req.getSession();
+        if((session!=null) && (session.getAttribute("user") !=null)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            //редирект на логин
+            res.sendRedirect(contextPath + "/signIn");
+        }
+    }
 }
